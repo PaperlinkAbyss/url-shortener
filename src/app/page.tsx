d91d85e1e7dcaa8b05399cf51f94a978c7f60192
John <z5512345z@gmail.com>
2024-03-28 20:54:00 +0100

    this should fix vercel deploy...?

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,8 @@ import AuthButtons from '@/components/AuthButtons'
 import Linker from '../components/Linker'
 
 export default function Home() {
-	// const cookiesValues = cookies().get('auth_session')
-	// const isValidSession = lucia.validateSession(cookiesValues.value)
-	// console.log('Is valid?', isValidSession)
 	return (
 		<div>
-			{/* {JSON.stringify(cookiesValues, null, 2)} */}
 			<AuthButtons />
 			<Linker />
 		</div>
