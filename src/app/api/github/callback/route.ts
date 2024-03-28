6a4e3f07c66cb401494abae64e93a5c7dfadb048
John <z5512345z@gmail.com>
2024-03-28 20:41:47 +0100

    okay vercel

--- src/app/api/github/callback/route.ts
+++ src/app/api/github/callback/route.ts
@@ -28,46 +28,46 @@ export async function GET(request: Request) {
 		console.log('What is this all', { tokens, githubUserResponse, existingUser })
 		// await db.table('user').where('github_id', '=', githubUser.id).get()
 		return
-		if (existingUser) {
-			const session = await lucia.createSession(existingUser.id, {})
-			const sessionCookie = lucia.createSessionCookie(session.id)
-			cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
-			return new Response(null, {
-				status: 302,
-				headers: {
-					Location: '/',
-				},
-			})
-		}
+		// if (existingUser) {
+		// 	const session = await lucia.createSession(existingUser.id, {})
+		// 	const sessionCookie = lucia.createSessionCookie(session.id)
+		// 	cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
+		// 	return new Response(null, {
+		// 		status: 302,
+		// 		headers: {
+		// 			Location: '/',
+		// 		},
+		// 	})
+		// }
 
-		const userId = generateId(15)
+		// const userId = generateId(15)
 
-		// Replace this with your own DB client.
-		await db.table('user').insert({
-			id: userId,
-			github_id: githubUser.id,
-			username: githubUser.login,
-		})
+		// // Replace this with your own DB client.
+		// await db.table('user').insert({
+		// 	id: userId,
+		// 	github_id: githubUser.id,
+		// 	username: githubUser.login,
+		// })
 
-		const session = await lucia.createSession(userId, {})
-		const sessionCookie = lucia.createSessionCookie(session.id)
-		cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
-		return new Response(null, {
-			status: 302,
-			headers: {
-				Location: '/',
-			},
-		})
+		// const session = await lucia.createSession(userId, {})
+		// const sessionCookie = lucia.createSessionCookie(session.id)
+		// cookies().set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes)
+		// return new Response(null, {
+		// 	status: 302,
+		// 	headers: {
+		// 		Location: '/',
+		// 	},
+		// })
 	} catch (e) {
 		// the specific error message depends on the provider
-		if (e instanceof OAuth2RequestError) {
-			// invalid code
-			return new Response(null, {
-				status: 400,
-			})
-		}
-		return new Response(null, {
-			status: 500,
-		})
+		// if (e instanceof OAuth2RequestError) {
+		// 	// invalid code
+		// 	return new Response(null, {
+		// 		status: 400,
+		// 	})
+		// }
+		// return new Response(null, {
+		// 	status: 500,
+		// })
 	}
 }
